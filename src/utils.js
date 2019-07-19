14200ead457de94250897ada6a7ffdc437bc4418
albertdm <albert-dm@outlook.com>
2019-07-19 00:06:03 -0300

    Docs and config

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,12 @@
+/**
+ * 
+ * @param {Array<any>} values Array of values
+ * @return {Array<Array<any>>} Returns an Array of pairs(arrays of 2 or 1 value)
+ * 
+ */
 const makePairs = (values) => {
     if (!Array.isArray(values)) throw Error('Values should be an Array');
-    
+
     return values.reduce(function (result, _, index, array) {
         if (index % 2 === 0)
             result.push(array.slice(index, index + 2));
